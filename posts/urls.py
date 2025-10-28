c5dbbb086b70ef7a481c26c02cdfcc09c72b3acb
sjahn <asj214@naver.com>
2025-10-28 10:49:29 +0900

    AI 활용

--- posts/urls.py
+++ posts/urls.py
@@ -1,20 +1,9 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, PostCommentViewSet
+from .views import PostViewSet
 
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'posts', PostViewSet)
 
-urlpatterns = [
-  path(r'posts/<int:post_id>/comments', PostCommentViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-  })),
-  path(r'posts/<int:post_id>/comments/<int:pk>', PostCommentViewSet.as_view({
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-  })),
-]
+urlpatterns = []
 urlpatterns += router.urls
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
-from .serializers import PostSerializer, PostCommentSerializer
-from comments.models import Comment
+from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -61,65 +60,5 @@ class PostViewSet(viewsets.ModelViewSet):
   def destroy(self, request, pk=None, *args, **kwargs):
     post = self.get_object(pk)
     post.delete()
-
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class PostCommentViewSet(viewsets.ModelViewSet):
-  permission_classes = [IsAuthenticatedOrReadOnly]
-  serializer_class = PostCommentSerializer
-  queryset = Comment.objects.prefetch_related('user').all()
-
-  def get_post_queryset(self, post_id=None):
-    try:
-      post = Post.objects.get(pk=post_id)
-      return post.comments.all()
-    except Post.DoesNotExist:
-      raise NotFound('Not Found')
-
-  def get_queryset(self):
-    return self.queryset
-
-  def get_object(self, pk=None):
-    try:
-      return self.queryset.get(pk=pk)
-    except Comment.DoesNotExist:
-      raise NotFound('Not Found')
-  
-  def list(self, request, post_id, *args, **kwargs):
-    page = self.paginate_queryset(self.get_post_queryset(post_id))
-    serializer = self.get_serializer(page, many=True)
-    return self.get_paginated_response(serializer.data)
-
-  def create(self, request, post_id, *args, **kwargs):
-    context = {
-      'user': request.user,
-      'post_id': post_id
-    }
-    serializer = self.serializer_class(
-      data=request.data,
-      context=context,
-    )
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-  def update(self, request, post_id=None, pk=None, *args, **kwargs):
-    comment = self.get_object(pk)
-    serializer = self.serializer_class(
-      comment,
-      data=request.data,
-      context={'user': request.user},
-      partial=True
-    )
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-
-    return Response(serializer.data)
-    
-  def destroy(self, request, post_id=None, pk=None, *args, **kwargs):
-    comment = self.get_object(pk)
-    comment.delete()
 
     return Response(status=status.HTTP_204_NO_CONTENT)
